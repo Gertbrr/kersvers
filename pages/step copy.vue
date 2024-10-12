f62b5b8d7d365bf52cc4633cc445b4bf0cdbdc53
Lennart Klein <lennart.klein@eo.nl>
2024-10-12 15:58:58 +0200

    style fixes

---
+++ pages/step copy.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { ref, computed } from "vue";
+import { store } from "~/store/store.js";
+import { generateResult } from "~/utils/chatgpt.js";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const currentStep = ref(0);
+const totalSteps = 5;
+
+const selectOption = (key) => {
+    console.log(store);
+    store.questionsAndAnswers[currentStep.value].answer = key;
+    nextStep();
+};
+
+async function nextStep() {
+    if (currentStep.value < totalSteps) {
+        currentStep.value++;
+    }
+
+    if (currentStep.value == 5) {
+        const result = await generateResult();
+
+        if (result) {
+            router.push("/result");
+        }
+    }
+}
+
+const isLastStep = computed(() => currentStep.value === totalSteps);
+</script>
+
+<template>
+    <div class="container mx-auto p-4 min-h-screen">
+        <FruitBackground />
+        <div class="text-center mb-4">
+            <img
+                src="~/assets/logo.svg"
+                class="w-24 mb-2 mx-auto lg:mb-8 lg:w-36"
+            />
+            <div class="bg-orange-200 rounded-full h-4 w-full">
+                <div
+                    class="bg-blue-500 h-4 rounded-full transition-all duration-300"
+                    :style="{ width: `${(currentStep / totalSteps) * 100}%` }"
+                ></div>
+            </div>
+        </div>
+        {{ currentStep }}
+        {{ store.questionsAndAnswers }}
+
+        <template v-if="currentStep < totalSteps">
+            <div class="relative">
+                <div
+                    class="relative text-center mb-8 bg-white p-8 rounded-md shadow-lg"
+                >
+                    <p class="-mt-2">{{ currentStep + 1 }}/{{ totalSteps }}</p>
+                    <h2 class="text-xl font-semibold">
+                        {{ store.questionsAndAnswers[currentStep].question }}
+                    </h2>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-2 gap-4">
+                <button
+                    v-for="(option, index) in store.questionsAndAnswers[
+                        currentStep
+                    ].options"
+                    :key="option"
+                    @click="selectOption(option)"
+                    class="transition duration-300 active:mt-1 active:border-b-0 text-white border-b-4 font-bold text-lg lg:text-2xl py-8 px-6 rounded-xl"
+                    :class="{
+                        'bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 border-blue-700':
+                            index == 0,
+                        'bg-green-500 hover:bg-green-600 focus:bg-green-600 border-green-700':
+                            index == 1,
+                        'bg-red-500 hover:bg-red-600 focus:bg-red-600 border-red-700':
+                            index == 2,
+                        'bg-yellow-500 hover:bg-yellow-600 focus:bg-yellow-600 border-yellow-700':
+                            index == 3,
+                    }"
+                >
+                    {{ option }}
+                </button>
+            </div>
+        </template>
+
+        <template v-else>Loading</template>
+
+        <!-- <div class="text-center mt-8">
+      <button
+        @click="nextStep"
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300"
+      >
+        {{ isLastStep ? 'Voltooien' : 'Volgende' }}
+      </button>
+    </div> -->
+    </div>
+</template>
--- pages/step.vue
+++ pages/step.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { ref, computed } from 'vue';
-import { store } from '~/store/store.js';
-import { generateResult, generateContext } from '~/utils/chatgpt.js';
-import { useRouter } from 'vue-router';
+import { ref, computed } from "vue";
+import { store } from "~/store/store.js";
+import { generateResult, generateContext } from "~/utils/chatgpt.js";
+import { useRouter } from "vue-router";
 
 const router = useRouter();
 
@@ -10,92 +10,87 @@ const currentStep = ref(0);
 const totalSteps = 5;
 
 const selectOption = (key) => {
-  console.log(store);
-  store.questionsAndAnswers[currentStep.value].answer = key;
-  nextStep();
+    console.log(store);
+    store.questionsAndAnswers[currentStep.value].answer = key;
+    nextStep();
 };
 
 async function nextStep() {
-  if (currentStep.value < totalSteps) {
-    currentStep.value++;
-  }
-
-  if (currentStep.value == 5) {
-    const result = await generateResult();
-
-    console.log(result, 'result');
-
-    if (result) {
-      const result2 = await generateContext();
-      console.log(result2, 'result2');
-      if (result2) {
-        router.push('/result');
-      }
+    if (currentStep.value < totalSteps) {
+        currentStep.value++;
     }
-  }
+
+    if (currentStep.value == 5) {
+        const result = await generateResult();
+
+        console.log(result, "result");
+
+        if (result) {
+            const result2 = await generateContext();
+            console.log(result2, "result2");
+            if (result2) {
+                router.push("/result");
+            }
+        }
+    }
 }
 
 const isLastStep = computed(() => currentStep.value === totalSteps);
 </script>
 
 <template>
-  <div class="container mx-auto p-4">
-    <div class="text-center mb-4">
-      <h1 class="text-2xl font-bold mb-2">Logo</h1>
-      <div class="bg-gray-200 rounded-full h-4 w-full">
-        <div
-          class="bg-blue-500 h-4 rounded-full transition-all duration-300"
-          :style="{ width: `${(currentStep / totalSteps) * 100}%` }"
-        ></div>
-      </div>
-    </div>
-
-    <template v-if="currentStep < totalSteps">
-      <div class="relative">
-        <div class="relative text-center mb-8 bg-white p-8 rounded-md shadow-lg">
-          <p class="mt-2">{{ currentStep + 1 }}/{{ totalSteps }}</p>
-          <h2 class="text-xl font-semibold">{{ store.questionsAndAnswers[currentStep].question }}</h2>
+    <div class="container mx-auto min-h-screen p-4">
+        <div class="text-center mb-4">
+            <img
+                src="~/assets/logo.svg"
+                class="w-24 mb-2 mx-auto lg:mb-8 lg:w-36"
+            />
+            <div class="bg-orange-200 rounded-full h-4 w-full">
+                <div
+                    class="bg-blue-500 h-4 rounded-full transition-all duration-300"
+                    :style="{
+                        width: `${((currentStep + 1) / totalSteps) * 100}%`,
+                    }"
+                ></div>
+            </div>
         </div>
 
-        <template v-if="currentStep < totalSteps">
-          <div class="relative">
-            <div class="relative text-center mb-8 bg-white p-8 rounded-md shadow-lg">
-              <p class="-mt-2">{{ currentStep + 1 }}/{{ totalSteps }}</p>
-              <h2 class="text-xl font-semibold">
-                {{ store.questionsAndAnswers[currentStep].question }}
-              </h2>
-            </div>
-          </div>
-
-          <div class="grid grid-cols-2 gap-4">
-            <button
-              v-for="(option, index) in store.questionsAndAnswers[currentStep].options"
-              :key="option"
-              @click="selectOption(option)"
-              class="transition duration-300 active:mt-1 active:border-b-0 text-white border-b-4 font-bold text-lg lg:text-2xl py-8 px-6 rounded-xl"
-              :class="{
-                'bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 border-blue-700': index == 0,
-                'bg-green-500 hover:bg-green-600 focus:bg-green-600 border-green-700': index == 1,
-                'bg-red-500 hover:bg-red-600 focus:bg-red-600 border-red-700': index == 2,
-                'bg-yellow-500 hover:bg-yellow-600 focus:bg-yellow-600 border-yellow-700': index == 3,
-              }"
+        <div class="relative">
+            <div
+                class="relative text-center mb-8 bg-white p-8 rounded-md shadow-lg"
             >
-              {{ option }}
-            </button>
-          </div>
-        </template>
+                <p class="mt-2">{{ currentStep + 1 }}/{{ totalSteps }}</p>
+                <h2 class="text-xl font-semibold">
+                    {{ store.questionsAndAnswers[currentStep].question }}
+                </h2>
+            </div>
 
-        <template v-else>Loading</template>
+            <template v-if="currentStep < totalSteps">
+                <div class="grid grid-cols-2 gap-4">
+                    <button
+                        v-for="(option, index) in store.questionsAndAnswers[
+                            currentStep
+                        ].options"
+                        :key="option"
+                        @click="selectOption(option)"
+                        class="transition duration-300 active:mt-1 active:border-b-0 text-white border-b-4 font-bold text-lg lg:text-2xl py-8 px-6 rounded-xl"
+                        :class="{
+                            'bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 border-blue-700':
+                                index == 0,
+                            'bg-green-500 hover:bg-green-600 focus:bg-green-600 border-green-700':
+                                index == 1,
+                            'bg-red-500 hover:bg-red-600 focus:bg-red-600 border-red-700':
+                                index == 2,
+                            'bg-yellow-500 hover:bg-yellow-600 focus:bg-yellow-600 border-yellow-700':
+                                index == 3,
+                        }"
+                    >
+                        {{ option }}
+                    </button>
+                </div>
+            </template>
 
-        <!-- <div class="text-center mt-8">
-      <button
-        @click="nextStep"
-        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300"
-      >
-        {{ isLastStep ? 'Voltooien' : 'Volgende' }}
-      </button>
-    </div> -->
-      </div>
-    </template>
-  </div>
+            <template v-else>Loading</template>
+        </div>
+    </div>
 </template>
