a475bb3781a5cf18bd2247826bc60c56370c35e3
Gert Boer <gert@embite.nl>
2024-10-12 12:14:55 +0200

    Data input

---
+++ python/index.py
@@ -0,0 +1,3 @@
+import pandas as pd
+csv_file = pd.DataFrame(pd.read_csv("data.csv", sep = ";", header = 0, index_col = False))
+csv_file.to_json("output.json", orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
